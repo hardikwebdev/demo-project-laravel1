d3770853a837db546a8da7c7eaf501683d04ffaf
Deepvasoya <deep.vasoya@aipxperts.com>
2021-11-12 14:03:48 +0530

    Fix Bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>{{config('app.name', 'Defix Finance ')}} | @yield('title','Home')</title>
+  <title>{{config('app.name', 'DefiXFinance ')}} | @yield('title','Home')</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ asset('assets/css/custom/feather.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/custom/themify-icons.css') }}">
