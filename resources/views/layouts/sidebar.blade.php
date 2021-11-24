1961d95266c1443015ce4898b3afb8b866e24237
Shital <shital.savaliya@aipxperts.com>
2021-11-24 11:22:24 +0530

    menu active changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,19 +1,19 @@
  <!-- partial:partials/_sidebar.html -->
  <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'dashboard') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('dashboard') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group848.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.dashboard')}}</span>
       </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'stacks' || Route::currentRouteName() == 'stakepool') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('stacks') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group955.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.staking_pools')}}</span>
       </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'node_register' || Route::currentRouteName() == 'node_management') ? 'active' : '' }}">
       <a class="nav-link collapsed" data-toggle="collapse" href="#node" aria-expanded="false" aria-controls="tables">
         <img src="{{ asset('assets/images/assets/Dashboard/Group953.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.nodes-management')}}</span>
@@ -21,18 +21,18 @@
       </a>
       <div class="collapse" id="node">
         <ul class="nav flex-column sub-menu rounded-bottom">
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'node_register') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('node_register') }}">{{trans('custom.register')}}</a>
           </li>
           @if(auth()->user()->staking_history->count() > 0 ) 
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'node_management') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('node_management') }}">{{trans('custom.nodes')}}</a>
           </li>
           @endif
         </ul>
       </div>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'crypto_wallets' || Route::currentRouteName() == 'yield_wallet' || Route::currentRouteName() == 'commission_wallet' || Route::currentRouteName() == 'nft_wallet') ? 'active' : '' }}">
       <a class="nav-link collapsed" data-toggle="collapse" href="#wallets" aria-expanded="false" aria-controls="tables">
         <img src="{{ asset('assets/images/assets/Dashboard/Group952.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.wallets')}}</span>
@@ -40,44 +40,44 @@
       </a>
       <div class="collapse" id="wallets">
         <ul class="nav flex-column sub-menu rounded-bottom">
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'crypto_wallets') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('crypto_wallets') }}">{{trans('custom.crypto_wallet')}}</a>
           </li>
           @if(auth()->user()->staking_history->count() > 0 ) 
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'yield_wallet') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('yield_wallet') }}">{{trans('custom.yield_wallet')}}</a>
           </li>
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'commission_wallet') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('commission_wallet') }}">{{trans('custom.commission-wallet')}}</a>
           </li>
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'nft_wallet') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('nft_wallet') }}">{{trans('custom.nft_wallet')}}</a>
           </li>
           @endif
         </ul>
       </div>
     </li>
-   <!--  <li class="nav-item">
+   <!--  <li class="nav-item {{ (Route::currentRouteName() == 'dashboard') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('nft_marketplace') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group951.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">NFT Marketplace </span>
       </a>
     </li> -->
     @if(auth()->user()->staking_history->count() > 0 ) 
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'withdrawal') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('withdrawal') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group850.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.withdrawals')}}</span>
       </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'ledger') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('ledger') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group956.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.Ledger')}}</span>
       </a>
     </li>
     @endif
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'account') ? 'active' : '' }}">
       <a class="nav-link collapsed" data-toggle="collapse" href="#account" aria-expanded="false" aria-controls="tables">
         <img src="{{ asset('assets/images/assets/Dashboard/Group851.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.accounts')}}</span>
@@ -85,17 +85,17 @@
       </a>
       <div class="collapse" id="account">
         <ul class="nav flex-column sub-menu rounded-bottom">
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'account') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('account') }}">{{trans('custom.my-account')}}</a>
           </li>
-          <!-- <li class="nav-item"> 
+          <!-- <li class="nav-item {{ (Route::currentRouteName() == 'dashboard') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('my_collection') }}">My Collection</a>
           </li> -->
         </ul>
       </div>
     </li>
     @if(auth()->user()->staking_history->count() > 0 ) 
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'news-and-events.index') ? 'active' : '' }}">
       <a class="nav-link collapsed" data-toggle="collapse" href="#news" aria-expanded="false" aria-controls="tables">
         <img src="{{ asset('assets/images/assets/Dashboard/Group2.png') }}" class="cus-sidebar-icon-news" alt="">
         <span class="menu-title">{{trans('custom.news')}}</span>
@@ -103,10 +103,10 @@
       </a>
       <div class="collapse" id="news">
         <ul class="nav flex-column sub-menu rounded-bottom">
-          <li class="nav-item"> 
+          <li class="nav-item {{ (Route::currentRouteName() == 'news-and-events.index') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('news-and-events.index') }}">{{trans('custom.news-events')}}</a>
           </li>
-          <!-- <li class="nav-item"> 
+          <!-- <li class="nav-item {{ (Route::currentRouteName() == 'dashboard') ? 'active' : '' }}"> 
             <a class="nav-link pl-0" href="{{ route('my_collection') }}">My Collection</a>
           </li> -->
         </ul>
@@ -114,14 +114,14 @@
     </li>
     @endif
     
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'help_support.index') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('help_support.index') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Path1214.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.help_support')}}</span>
       </a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item {{ (Route::currentRouteName() == 'helpandfaq') ? 'active' : '' }}">
       <a class="nav-link" href="{{ route('helpandfaq') }}">
         <img src="{{ asset('assets/images/assets/Dashboard/Group1.png') }}" class="cus-sidebar-icon" alt="">
         <span class="menu-title">{{trans('custom.help_and_faq')}}</span>
