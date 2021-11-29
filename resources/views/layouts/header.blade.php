568f17d9cf53db522160004155cdffa352a7acc8
shital.savaliya@aipxperts.com <shital.savaliya@aipxperts.com>
2021-11-29 11:20:28 +0530

    live font issue

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,24 +17,25 @@
         <div class="ml-4 mr-4">
           <div class="">
             <div class="navigation-cus">
-               <div class="cus-dropdown text-right mb-3 select-lang-de mt-3">
-                  <select style=" height:35px;" class="form-control cus-bg-tra-b" data-width="fit"
-                     onchange="javascript:window.location.href='<?php echo $local_url; ?>/'+this.value;">
-                     <option <?php if(app()->getLocale() == 'en'){ echo 'selected' ;} ?> value="en"
-                        data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
-                     <option <?php if(app()->getLocale() == 'cn'){ echo 'selected' ;} ?> value="cn"
-                        data-content='<span class="flag-icon flag-icon-cn"></span> China'>中文(Chinese)</option>
-                  </select>
-               </div>
+             <div class="cus-dropdown text-right mb-3 select-lang-de mt-3">
+              <select style=" height:35px;" class="form-control cus-bg-tra-b" data-width="fit"
+              onchange="javascript:window.location.href='<?php echo $local_url; ?>/'+this.value;">
+              <option <?php if(app()->getLocale() == 'en'){ echo 'selected' ;} ?> value="en"
+                data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
+                <option <?php if(app()->getLocale() == 'cn'){ echo 'selected' ;} ?> value="cn"
+                  data-content='<span class="flag-icon flag-icon-cn"></span> China'>中文(Chinese)</option>
+                </select>
+              </div>
             </div>
-        </div>
+          </div>
         </div>
         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+          <img src="{{auth()->user()->profile_image}}" alt="">
         </a>
-        <div class="ml-2">
-          <h4 class="text-warning font-weight-bold mb-0">{{auth()->user()->name}}</h4>
-          <!-- <span>Active</span> -->
-        </div>
+          <div class="ml-2">
+            <h4 class="text-warning font-weight-bold mb-0">{{auth()->user()->name}}</h4>
+            <!-- <span>Active</span> -->
+          </div>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
           {{-- <a class="dropdown-item">
             <i class="ti-settings text-primary"></i>
@@ -45,8 +46,8 @@
             {{trans('custom.logout')}}
           </a>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                      </form>
+            @csrf
+          </form>
         </div>
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
           <span class="icon-menu"></span>
