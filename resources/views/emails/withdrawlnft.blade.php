08e0aeecc0bfeb508d7e3268379a1175af5db135
shital.savaliya@aipxperts.com <shital.savaliya@aipxperts.com>
2021-12-01 15:31:40 +0530

    nft withdrawal changes from sheet

---
+++ resources/views/emails/withdrawlnft.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<head>
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Welcome to DefixFinance</title>
+  <link href='http://fonts.cdnfonts.com/css/helvetica-neue-9' rel='stylesheet' type='text/css'>
+  <style type="text/css">
+    @import url('http://fonts.cdnfonts.com/css/helvetica-neue-9');
+    html {
+      width: 100% !important;
+    }
+    
+    body {
+      width: 100% !important;
+      margin: 0;
+      padding: 0;
+      font-family: 'Helvetica Neue';
+    }
+    
+    .ReadMsgBody {
+      width: 100%;
+      background-color: #d3dbe3;
+    }
+    
+    .ExternalClass {
+      width: 100%;
+      background-color: #d3dbe3;
+    }
+    
+    a img,
+    img {
+      line-height: 100%;
+      outline: 0;
+      text-decoration: none;
+      margin: 0;
+      border: 0;
+      -ms-interpolation-mode: bicubic;
+      max-width: 100%;
+    }
+    
+    @media only screen and (max-width: 576px  ) {
+      body table table {
+        width: 100% !important;
+      }
+      body table [class="wd-auto"] {
+        width: auto !important;
+      }
+      body td[class="header-center-pad5"] {
+        display: block !important;
+        width: 100% !important;
+        text-align: center !important;
+        padding: 5px 0px !important;
+      }
+      body td[class="top-link"],
+      body td[class="in-block"] {
+        display: block !important;
+        width: 100% !important;
+        padding: 0px !important;
+      }
+      body td[class="box"] {
+        display: block !important;
+      }
+      body td a[class=menu] {
+        padding: 0px 1.5% !important;
+        font-size: 12px !important;
+      }
+      body td [class="td-pad-20"] {
+        padding: 20px !important;
+      }
+      body td [class="pad-T20"] {
+        padding-top: 20px !important;
+      }
+      body td [class="pad-B20"] {
+        padding-bottom: 20px !important;
+      }
+      body td [class="pad-LR"] {
+        padding-left: 20px !important;
+        padding-right: 20px !important;
+      }
+      body td [class="pad-TB"] {
+        padding-top: 20px !important;
+        padding-bottom: 20px !important;
+      }
+      body td [class="pad-LRB"] {
+        padding-left: 20px !important;
+        padding-right: 20px !important;
+        padding-bottom: 20px !important;
+      }
+      body table [class="mob-hidden"] {
+        display: none !important;
+      }
+      body td [class="size-18"] {
+        font-size: 18px !important;
+        padding-top: 20px !important
+      }
+      body td [class="size-20"] {
+        font-size: 20px !important;
+        line-height: 25px !important;
+      }
+      body td[class="key-list"] {
+        display: block !important;
+        width: 100% !important;
+      }
+    }
+
+    @media only screen and (min-width: 576px) {
+      .pt-md-40{
+        padding-top: 40px;
+      }
+    }
+  </style>
+</head>
+
+<body style="padding:0px; margin:0; background:#e6e9ec;">
+  <!-- Start Template -->
+  <table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#e6e9ec">
+    <tr>
+      <td align="center" valign="top">
+        <table cellpadding="0" cellspacing="0" border="0" width="900" align="center">
+          <tr>
+            <td class="pad-LRB" align="center" valign="top" bgcolor="#547be9" style="background-image: url({{ asset('images/email/background.jpg') }}); -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover; background-position:center top; background-repeat:no-repeat; padding:0px;">
+                            <!--[if gte mso 9]>
+                                <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;min-height:2392px;mso-position-horizontal:center;">
+                                    <v:fill type="tile" src="images/wc-bg.jpg" color="#eaeaea"  />
+                                </v:background>
+                              <![endif]-->
+                                <!-- Start Logo -->
+
+                                <table cellpadding="0" cellspacing="0" border="0" width="800" align="center">
+                                  <tr>
+                                    <td align="center" style="padding:45px 0px;" class="td-pad-20">
+                                      <a style="text-decoration:none; outline:none;" href="#" title="Defix">
+                                        <img src="{{ asset('images/email/defixfinance-logo-white.png')}}" width="350" style="display:block;" />
+                                      </a>
+                                    </td>
+                                  </tr>
+                                </table>
+
+                                
+                                <table cellpadding="0" cellspacing="0" border="0" width="800" align="center">
+                                  <tr>
+                                    <td align="center">
+                                      <a style="text-decoration:none; outline:none;" href="#" title="Defix">
+                                        <img src="{{ asset('images/email/banner.jpg')}}" width="100%" style="display:block;" />
+                                      </a>
+                                    </td>
+                                  </tr>
+                                </table>
+
+                                <!-- End Logo -->
+                                <!-- Start Header -->
+
+                                <table width="800" cellpadding="0" cellspacing="0" border="0" align="center">
+                                  <tr>
+                                    <td align="center" style="background:#ffffff; padding:10px 25px 40px;" bgcolor="#ffffff" class="td-pad-20">
+                                      <table align="center" cellpadding="0" cellspacing="0" border="0" width="100%">
+
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </table>
+
+                                <!-- End Header -->
+                                <!-- Start Welcome -->
+                                <table width="800" cellpadding="0" cellspacing="0" border="0" align="center">
+                                  <tr>
+                                    <td align="center" style="background:#ffffff; padding:0px 40px 40px;" bgcolor="#ffffff" class="pad-LRB">
+                                      <table align="center" cellpadding="0" cellspacing="0" border="0" width="100%">
+
+                                        <tr>
+                                          <td>
+                                            <table align="center" cellpadding="0" cellspacing="0" border="0" width="500">
+                                              <tr>
+                                                <td class="size-20" align="center" style="font-weight:600; font-size:28px; color:black; font-family:Helvetica; mso-line-height-rule: exactly; text-align:center; line-height:42px;">DefiXFinance NFT Withdrawal Verification</td>
+                                              </tr>
+                                              <tr>
+                                                <td class="pad-B20" align="center" style="font-weight:400; font-size:14px; color:#5c5c5c; font-family:Helvetica; mso-line-height-rule: exactly; line-height:26px; padding:10px 0px 10px;">Dear Esteemed Client, <br/>
+
+                                                A NFT withdrawal request has been made on your account. If you want to continue, please click the link below. If it is not you, please login your account and change your password immediately</td>
+                                              </tr>
+                                              <tr>
+                                                <td class="pad-B20" align="center" style="font-weight:bold; font-size:16px; color:black; font-family:Helvetica; mso-line-height-rule: exactly; line-height:26px; padding:10px 0px 10px;">
+
+                                                  <a href="{{$routeUrl}}">I confirm my request to withdraw NFT</a>
+                                                </td>
+                                              </tr>
+
+                                            </table>
+                                          </td>
+                                        </tr>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </table>
+                                <!-- End Welcome -->
+                                
+
+                                <!-- End Features -->
+                                
+                                
+                                <!-- Start Footer-->
+                                <table align="center" cellpadding="0" cellspacing="0" border="0" width="800">
+                                  <tr>
+                                    <td align="center" valign="top" class="" style="padding:20px; background:#231f20; border-radius:0px 0px 4px 4px" bgcolor="#152922">
+                                      <table cellpadding="0" cellspacing="0" border="0">
+                                        <tr>
+                                          <td align="left" valign="top">
+                                            <table cellpadding="0" cellspacing="0" border="0" align="left" class="wd-auto">
+                                              <td style="font-family:Helvetica;font-size:9px;color:#bcbcbc"><strong>Disclaimer:</strong> DefiXFinance has made every attempt to ensure the accuracy and reliability of the information provided on this website. The information is provided “as is” without warranty of any kind. DefiXFinance makes no representations and gives no warranties of whatever nature in respect, including but not limited to the accuracy or completeness of any information, facts and/or opinions contained therein.
+                                                <br>
+                                                <br>
+                                                Your use of the service is at your sole risk. The service is provided on an "As is" and "As available" basis. Except as otherwise expressly provided herein, DefiXFinance expressly disclaims all warranties of any kind, whether express, implied or statutory, including, but not limited to the implied warranties of merchantability, fitness for a particular purpose, title, and non-infringement.
+                                              </table>
+                                            </td>
+
+                                          </tr>
+                                        </table>
+                                      </td>
+                                    </tr>
+                                  </table>
+                                  <!-- End Footer-->
+                                  <!-- Start Footer Space-->
+                                  <table width="800" cellspacing="0" cellpadding="0" border="0" align="center">
+                                    <tr>
+                                      <td style="height:100px;" height="100" class="mob-hidden" valign="top" align="center">&nbsp;</td>
+                                    </tr>
+                                  </table>
+                                  <!-- End Footer Space-->
+
+
+                                </td>
+                              </tr>
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+                      <!-- End Template -->
+                    </body>
+
+                    </html>
